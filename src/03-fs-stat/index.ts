60a8566aae7ff8512000854bc237e9e2ee376479
damien-bernard <155372850+damien-bernard@users.noreply.github.com>
2024-03-03 17:45:30 +0100

    fix build

--- src/03-fs-stat/index.ts
+++ src/03-fs-stat/index.ts
@@ -5,22 +5,22 @@ import fs from 'fs'
 const path: string = process.argv.length === 3 ? process.argv[2] : '.'
 
 fs.readdir(path, (err, files) => {
-
   if (err != null) {
     console.error(err)
     return
   }
 
   files.forEach((file) => {
-    const fullPath = `${path}/${file}`;
+    const fullPath = `${path}/${file}`
     fs.stat(fullPath, (err, stats) => {
-      if (err) {
-        console.error('Error getting file stats:', err);
-        return;
+      if (err != null) {
+        console.error('Error getting file stats:', err)
+        return
       }
 
-      console.log(`File: ${file}, Type: ${stats.isDirectory() ? 'Directory' : 'File'}, Size: ${stats.size} bytes`);
-    });
+      console.log(
+        `File: ${file}, Type: ${stats.isDirectory() ? 'Directory' : 'File'}, Size: ${stats.size} bytes`,
+      )
+    })
   })
-
 })
